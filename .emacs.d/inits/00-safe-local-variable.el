e1e176061ff3ce635098854cca25da2c49b23a72
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-08-17 19:43:44 +0900

    emacs: マジックコメントがあっても警告を出さないように設定を追加

---
+++ .emacs.d/inits/00-safe-local-variable.el
@@ -0,0 +1,8 @@
+;; マジックコメントがあっても警告を出さないようにする
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(frame-background-mode nil)
+ '(safe-local-variable-values (quote ((encoding . utf-8)))))
