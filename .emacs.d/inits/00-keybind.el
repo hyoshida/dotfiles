19e438d5a3951b340f92a3c68dc00d658d88bf9d
YOSHIDA Hiroki <hyoshida@appirits.com>
2013-08-18 02:26:50 +0900

    emacs: C-wの挙動をシェル風に変更

--- .emacs.d/inits/00-keybind.el
+++ .emacs.d/inits/00-keybind.el
@@ -10,3 +10,12 @@
   (switch-to-buffer
     (generate-new-buffer "*untitled*")))
 (global-set-key (kbd "C-x n") 'generate-temporary-buffer)
+
+;; C-w の挙動をシェルに寄せる
+;; from http://qiita.com/k_ui/items/e6978008253ba70c037c
+(defun backward-kill-word-or-kill-region ()
+  (interactive)
+  (if (or (not transient-mark-mode) (region-active-p))
+      (kill-region (region-beginning) (region-end))
+    (backward-kill-word 1)))
+(global-set-key (kbd "C-w") 'backward-kill-word-or-kill-region)
