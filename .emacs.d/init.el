2be24b11b62b5d4f1f437a7a9aa00bc00b6b03b8
YOSHIDA Hiroki <hyoshida@appirits.com>
2014-08-11 05:10:09 +0900

    emacs: packageはemacs24からビルトインになったのでsite-lispのものを読み込まないように修正

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,10 @@
 ;; ~/.emacs.d/site-lisp を load-path に追加
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
-  (add-to-list 'load-path default-directory)
+  ;; TODO: バージョンごとにディレクトリを分ける
+  (if (<= emacs-major-version 24)
+    (add-to-list 'load-path (concat default-directory "/init-loader"))
+    (add-to-list 'load-path default-directory)
+    )
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
     (normal-top-level-add-subdirs-to-load-path)))
 
