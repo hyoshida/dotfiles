0cbb21ecf277eb88e2cadc5f61772c75f75d7333
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-07-16 16:40:06 +0900

    emacs: Remap evil-mode toggle key

--- .emacs.d/inits/30-evil-mode.el
+++ .emacs.d/inits/30-evil-mode.el
@@ -1,6 +1,11 @@
-(require 'evil-maps)
+; Remap evil-mode toggle key from C-z to C-x `
+; http://askubuntu.com/questions/99160/how-to-remap-emacs-evil-mode-toggle-key-from-ctrl-z
+(setq evil-toggle-key "C-x `")
+(global-set-key (kbd evil-toggle-key) 'evil-mode)
 
 ; for helm
+(require 'evil-maps)
+
 (when (require 'helm-config nil t)
   (define-key evil-normal-state-map (kbd "SPC f f") 'helm-find-files)
   (define-key evil-normal-state-map (kbd "SPC f m") 'helm-recentf)
