d671b107a658d2a1f4b9ba0b84efd6d5357f1227
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-07-16 18:47:50 +0900

    emacs: Change keybinds for helm

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -1,2 +1,5 @@
 ;; refs https://github.com/emacs-helm/helm#basic-usage
 (helm-mode 1)
+
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
+(define-key helm-find-files-map (kbd "C-w") 'helm-find-files-up-one-level)
