1900a99d51a562495aaa22b426d68d7610eea260
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-07-17 13:22:12 +0900

    emacs: Add the setting to don't insert magic comment

--- .emacs.d/inits/30-ruby-mode.el
+++ .emacs.d/inits/30-ruby-mode.el
@@ -1,3 +1,6 @@
 (add-hook 'ruby-mode-hook
   (lambda ()
     (modify-syntax-entry ?_ "w" ruby-mode-syntax-table)))
+
+;; Don't insert encoding magic comment
+(setq ruby-insert-encoding-magic-comment nil)
