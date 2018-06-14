ac7174749e3f44befcb3f4952a0409d78579fade
YOSHIDA Hiroki <hyoshida@appirits.com>
2018-06-14 13:57:22 +0900

    CSVファイルを自動整形の対象外にする

--- .git_template/hooks/.pre-commit.sh
+++ .git_template/hooks/.pre-commit.sh
@@ -12,7 +12,7 @@ now=$(date '+%Y%m%d%H%M%S')
 # Find files with trailing whitespace
 for file in `exec git diff-index --check --cached $against -- | sed '/^[+-]/d' | cut -f1 -d: | uniq`; do
   # Ignore .unity, .meta and .asset file
-  if echo $file | grep -e '\.unity$' -e '\.meta$' -e '\.asset$'; then
+  if echo $file | grep -e '\.unity$' -e '\.meta$' -e '\.asset$' -e '\.csv$'; then
     continue;
   fi
 
