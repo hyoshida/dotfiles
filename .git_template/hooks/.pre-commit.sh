f40b701060d5df3131b02ad3b19bd13b83364564
YOSHIDA Hiroki <hyoshidash@gmail.com>
2023-03-15 14:59:26 +0900

    コミット時に勝手にファイルの中身を書き換えるのは遅いしリスキーなのでやめる
    とくにUnity触っているときに使い物にならない。

--- .git_template/hooks/.pre-commit.sh
+++ .git_template/hooks/.pre-commit.sh
@@ -7,29 +7,6 @@ else
   against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
 fi
 
-now=$(date '+%Y%m%d%H%M%S')
-
-# Find files with trailing whitespace
-for file in `exec git diff-index --check --cached $against -- | sed '/^[+-]/d' | cut -f1 -d: | uniq`; do
-  # Ignore .unity, .meta, .prefab and .asset file
-  if echo $file | grep -e '\.unity$' -e '\.meta$' -e '\.prefab$' -e '\.asset$' -e '\.csv$'; then
-    continue;
-  fi
-
-  # Backup changes
-  mv -f "$file" "$file.save"
-  git checkout -- "$file"
-
-  # Remove trailing whitespace
-  sed -i.$now 's/[[:space:]]*$//' "$file"
-  rm "$file.$now"
-  git add $file
-
-  # Restore changes
-  sed 's/[[:space:]]*$//' "$file.save" > "$file"
-  rm "$file.save"
-done
-
 # Change the file permissions (only Windows)
 case "$OSTYPE" in
   *cygwin*)
